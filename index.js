c4ff1addb3d94edc99a3f9a3273cc5ece84ef9d5
Thai Nguyen <nh0kcrazy04@gmail.com>
2025-08-10 11:13:58 +0700

    change something

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    var message = 'It works!\n',
+    var message = 'It works 13 123 123 123!\n',
         version = 'NodeJS ' + process.versions.node + '\n',
         response = [message, version].join('\n');
     res.end(response);
